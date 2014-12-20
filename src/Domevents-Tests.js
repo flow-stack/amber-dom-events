6f6a49c90ea97d4b61582142b4b48a089f4ba5c7
Sebastian Sastre <sebastian@flowingconcept.com>
2014-12-20 16:09:44 -0200

    adjusts camelization in the name and cleans up default thigns and namespace

--- src/Domevents-Tests.js
+++ src/Domevents-Tests.js
@@ -1,9 +1,9 @@
-define("amber-domevents/Domevents-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
+define("domevents/DOMEvents-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
 var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
 var smalltalk=$core,_st=$recv,globals=$globals;
-$core.addPackage('Domevents-Tests');
-$core.packages["Domevents-Tests"].transport = {"type":"amd","amdNamespace":"amber-domevents"};
+$core.addPackage('DOMEvents-Tests');
+$core.packages["DOMEvents-Tests"].transport = {"type":"amd","amdNamespace":"domevents"};
 
-$core.addClass('DomeventsTest', $globals.TestCase, [], 'Domevents-Tests');
+$core.addClass('DOMEventsTest', $globals.TestCase, [], 'DOMEvents-Tests');
 
 });
