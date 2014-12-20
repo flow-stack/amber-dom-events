af3dff0d39e9dfd6e5e336cb8ba5aec36c673b11
Sebastian Sastre <sebastian@flowingconcept.com>
2014-12-20 16:00:51 -0200

    installing

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-amber-dom-events
-================
+# DOMEvents
 
-Use custom DOM events in any Amber object
+The Application or The Library doing The Thing.
+
+## Getting Started
+
+Brought project alive (for example after `git clone`):
+
+```sh
+npm install
+bower install
+grunt devel
+```
+
+Developing the project (after brought alive):
+ 
+Start server with `amber serve` and go to `http://localhost:4000/` in your browser and follow the instructions
---
+++ src/Domevents-Tests.js
@@ -0,0 +1,9 @@
+define("amber-domevents/Domevents-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
+var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
+var smalltalk=$core,_st=$recv,globals=$globals;
+$core.addPackage('Domevents-Tests');
+$core.packages["Domevents-Tests"].transport = {"type":"amd","amdNamespace":"amber-domevents"};
+
+$core.addClass('DomeventsTest', $globals.TestCase, [], 'Domevents-Tests');
+
+});
