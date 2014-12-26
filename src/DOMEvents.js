6a16dd06ef43e4e0ded77941e743271ff8b53f7b
Sebastian Sastre <sebastian@flowingconcept.com>
2014-12-26 17:22:20 -0200

    removes use of $ in favor of asjQuery; many methods that were in native js are in st now

--- src/DOMEvents.js
+++ src/DOMEvents.js
@@ -17,12 +17,85 @@ return self;
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: [],
-source: "start\x0a\x0a\x09",
+source: "start",
 referencedClasses: [],
 //>>excludeEnd("ide");
 messageSends: []
 }),
 $globals.DOMEvents.klass);
+
+$core.addMethod(
+$core.method({
+selector: "asJQuery",
+protocol: '*DOMEvents',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) { 
+//>>excludeEnd("ctx");
+var $1;
+$1=$recv(jQuery)._value_(self);
+return $1;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"asJQuery",{},$globals.Object)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "asJQuery\x0a\x09\x0a\x09^ jQuery value: self",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["value:"]
+}),
+$globals.Object);
+
+$core.addMethod(
+$core.method({
+selector: "off",
+protocol: '*DOMEvents',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) { 
+//>>excludeEnd("ctx");
+$recv(self._asJQuery())._off();
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"off",{},$globals.Object)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "off\x0a\x09\x22Unwires the observation of this instance to the occurrence of all events.\x0a\x09All handlers will stop reacting\x22\x0a\x0a\x09self asJQuery off",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["off", "asJQuery"]
+}),
+$globals.Object);
+
+$core.addMethod(
+$core.method({
+selector: "off:",
+protocol: '*DOMEvents',
+fn: function (anEventName){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) { 
+//>>excludeEnd("ctx");
+$recv(self._asJQuery())._off_(anEventName);
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"off:",{anEventName:anEventName},$globals.Object)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: ["anEventName"],
+source: "off: anEventName \x0a\x09\x22Unwires the observation of this instance from the occurrence of anEventName.\x0a\x09All handlers for it will stop reacting\x22\x0a\x0a\x09self asJQuery off: anEventName",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["off:", "asJQuery"]
+}),
+$globals.Object);
 
 $core.addMethod(
 $core.method({
@@ -33,18 +106,19 @@ var self=this;
 //>>excludeStart("ctx", pragmas.excludeDebugContexts);
 return $core.withContext(function($ctx1) { 
 //>>excludeEnd("ctx");
-return $(self).trigger(anEventName);
-return self;
+var $1;
+$1=$recv(self._asJQuery())._trigger_(anEventName);
+return $1;
 //>>excludeStart("ctx", pragmas.excludeDebugContexts);
 }, function($ctx1) {$ctx1.fill(self,"trigger:",{anEventName:anEventName},$globals.Object)});
 //>>excludeEnd("ctx");
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: ["anEventName"],
-source: "trigger: anEventName \x0a\x09\x22Triggers anEventName from this instance (no arguments).\x0a\x09For example:  self trigger: #changed.\x22\x0a\x09\x0a\x09<return $(self).trigger(anEventName)>",
+source: "trigger: anEventName \x0a\x09\x22Triggers anEventName from this instance (no arguments).\x0a\x09For example:  self trigger: #changed.\x22\x0a\x09\x0a\x09^ self asJQuery trigger: anEventName",
 referencedClasses: [],
 //>>excludeEnd("ide");
-messageSends: []
+messageSends: ["trigger:", "asJQuery"]
 }),
 $globals.Object);
 
@@ -109,18 +183,19 @@ var self=this;
 //>>excludeStart("ctx", pragmas.excludeDebugContexts);
 return $core.withContext(function($ctx1) { 
 //>>excludeEnd("ctx");
-return $(self).trigger( anEventName, someArguments );
-return self;
+var $1;
+$1=$recv(self._asJQuery())._trigger_with_(anEventName,someArguments);
+return $1;
 //>>excludeStart("ctx", pragmas.excludeDebugContexts);
 }, function($ctx1) {$ctx1.fill(self,"trigger:withAll:",{anEventName:anEventName,someArguments:someArguments},$globals.Object)});
 //>>excludeEnd("ctx");
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: ["anEventName", "someArguments"],
-source: "trigger: anEventName withAll: someArguments\x0a\x09\x22Triggers anEventName from this instance using all the given arugments\x22\x0a\x0a\x09<return $(self).trigger( anEventName, someArguments )>",
+source: "trigger: anEventName withAll: someArguments\x0a\x09\x22Triggers anEventName from this instance using all the given arugments\x22\x0a\x0a\x09^ self asJQuery trigger: anEventName with: someArguments",
 referencedClasses: [],
 //>>excludeEnd("ide");
-messageSends: []
+messageSends: ["trigger:with:", "asJQuery"]
 }),
 $globals.Object);
 
@@ -133,8 +208,8 @@ var self=this;
 //>>excludeStart("ctx", pragmas.excludeDebugContexts);
 return $core.withContext(function($ctx1) { 
 //>>excludeEnd("ctx");
-$(self).on(anEventName, function(e){
-		var someArguments = $(arguments).slice();
+jQuery(self).on(anEventName, function(e){
+		var someArguments = jQuery(arguments).slice();
 		someArguments.splice(0,1);
 		aReactionBlock.apply(null, someArguments)});
 return self;
@@ -144,7 +219,7 @@ return self;
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: ["anEventName", "aReactionBlock"],
-source: "when: anEventName do: aReactionBlock\x0a\x09\x22Wires the observation of this instance to react to the eventual occurrence of anEventName.\x0a\x09When (and if) anEventName happens, aReactionBlock will take place. \x0a\x09Based on this feature: http://forum.jquery.com/topic/triggering-custom-events-on-js-objects\x0a\x09Note that some objects wont fire (anArray for example).\x22\x0a\x0a\x09<$(self).on(anEventName, function(e){\x0a\x09\x09var someArguments = $(arguments).slice();\x0a\x09\x09someArguments.splice(0,1);\x0a\x09\x09aReactionBlock.apply(null, someArguments)})>",
+source: "when: anEventName do: aReactionBlock\x0a\x09\x22Wires the observation of this instance to react to the eventual occurrence of anEventName.\x0a\x09When (and if) anEventName happens, aReactionBlock will take place. \x0a\x09Based on this feature: http://forum.jquery.com/topic/triggering-custom-events-on-js-objects\x0a\x09Note that some objects wont fire (anArray for example).\x22\x0a\x0a\x09<jQuery(self).on(anEventName, function(e){\x0a\x09\x09var someArguments = jQuery(arguments).slice();\x0a\x09\x09someArguments.splice(0,1);\x0a\x09\x09aReactionBlock.apply(null, someArguments)})>",
 referencedClasses: [],
 //>>excludeEnd("ide");
 messageSends: []
